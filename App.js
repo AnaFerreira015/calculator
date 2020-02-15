136509f7695817c1f9bde11a04893e09c49c16a0
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-02-15 14:48:50 -0300

    Fixes bugs

--- App.js
+++ App.js
@@ -19,10 +19,14 @@ export default class App extends Component {
   state = { ...initialState }
 
   addDigit = n => {
-    if (n === "." && this.state.displayValue.includes(".")) return
+    console.debug(typeof this.state.displayValue)
 
     const clearDisplay = this.state.displayValue === "0"
       || this.state.clearDisplay
+
+    if (n === "." && !clearDisplay 
+      && this.state.displayValue.includes(".")) return
+    
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
     this.setState({ displayValue, clearDisplay: false })
@@ -54,7 +58,7 @@ export default class App extends Component {
 
       values[1] = 0
       this.setState({
-        displayValue: values[0],
+        displayValue: `${values[0]}`,
         operation: equals ? null : operation,
         current: equals ? 0 : 1,
         // clearDisplay: !equals,
