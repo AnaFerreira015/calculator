7bc61a910eab375754e5de153def98a768d6794a
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-02-15 14:21:54 -0300

    Corrects change of state

--- App.js
+++ App.js
@@ -25,19 +25,18 @@ export default class App extends Component {
       || this.state.clearDisplay
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
-
-    this.state({ displayValue, clearDisplay: false })
+    this.setState({ displayValue, clearDisplay: false })
 
     if (n !== ".") {
       const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
       values[this.state.current] = newValue
-      this.state({ values })
+      this.setState({ values })
     }
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: 0 })
+    this.setState({ ...initialState })
   }
 
   setOperation = operation => {
